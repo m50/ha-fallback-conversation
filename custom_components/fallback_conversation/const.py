2b38610ddf0f6746f1394cf1d39fceb4ee9dcd5c
rhyswaywood <rhys@waywood.me>
2024-04-02 00:18:41 +0800

    Add CONVERSATION_FINISHED event to const.py

--- custom_components/fallback_conversation/const.py
+++ custom_components/fallback_conversation/const.py
@@ -10,5 +10,7 @@ DEBUG_LEVEL_NO_DEBUG = "none"
 DEBUG_LEVEL_LOW_DEBUG = "low"
 DEBUG_LEVEL_VERBOSE_DEBUG = "verbose"
 
+EVENT_CONVERSATION_FINISHED = "fallback_conversation.conversation.finished"
+
 DEFAULT_NAME = "Fallback Conversation Agent"
 DEFAULT_DEBUG_LEVEL = DEBUG_LEVEL_NO_DEBUG
